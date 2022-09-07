3dcc0065d73627672cfc47de0bc6241948046244
hajin <f-a-k@naver.com>
2022-09-07 21:12:43 +0900

    기본 뼈대 구축

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
